976a780f57efdaf33a2976f9a118c048cd91fad5
StrIvanSer <svet42@inbox.ru>
2020-12-12 18:59:08 +0500

    Module 2: Lesson 2 Актуализация проекта, прохождение второго урока

---
+++ src/main/java/org/example/WebAppInitializer.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import org.apache.log4j.Logger;
+import org.example.web.config.WebContextConfig;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class WebAppInitializer implements WebApplicationInitializer {
+
+    Logger logger = Logger.getLogger(WebAppInitializer.class);
+
+    @Override
+    public void onStartup(javax.servlet.ServletContext servletContext) throws ServletException {
+
+        logger.info("loading app config");
+        XmlWebApplicationContext appContext = new XmlWebApplicationContext();
+        appContext.setConfigLocation("classpath:app-config.xml");
+        servletContext.addListener(new ContextLoaderListener(appContext));
+
+        logger.info("loading web config");
+//        XmlWebApplicationContext wenContext = new XmlWebApplicationContext();
+//        wenContext.setConfigLocation("classpath:web-config.xml");
+        AnnotationConfigWebApplicationContext wenContext = new AnnotationConfigWebApplicationContext();
+        wenContext.register(WebContextConfig.class);
+
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(wenContext);
+
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", dispatcherServlet);
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+        logger.info("dispatcher ready");
+
+    }
+
+}
